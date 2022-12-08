060c9dc21314a5884577b2bbf13f06f0af15fcf8
shakes24 <cherrycrisp0@gmail.com>
2022-12-08 15:08:09 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,11 +4,11 @@
 
 /**
  *
- *  * main - prints the size of various types
+ * main - prints the size of various types
  *
- *   * Return: 0 if exited properly, non-zero otherwise
+ * Return: 0 if exited properly, non-zero otherwise
  *
- *    */
+ */
 
 int main(void)
 
