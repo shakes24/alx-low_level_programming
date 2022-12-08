ff256ce13fe180e5e3cdb5ca02c79f104fafabb0
shakes24 <cherrycrisp0@gmail.com>
2022-12-08 14:43:11 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
-/*Write a C program that prints exactly */
+
+
+/**
+ *
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
 
 int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
+
+
