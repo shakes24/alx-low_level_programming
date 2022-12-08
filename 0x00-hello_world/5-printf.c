b49a52debfc9c5d976f2d315f35ebcb6e5bbf210
shakes24 <cherrycrisp0@gmail.com>
2022-12-08 15:21:58 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the function
+ * return: 0 if exited properly
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
