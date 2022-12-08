b138788923232d0514c1dda5694e4a7084787a51
shakes24 <cherrycrisp0@gmail.com>
2022-12-08 14:23:54 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /*Write a C program that prints exactly */
+
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
