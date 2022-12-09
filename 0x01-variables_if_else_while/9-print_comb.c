862268a67bf0a1c570558058dba8eb92b24e22fb
shakes24 <cherrycrisp0@gmail.com>
2022-12-09 10:35:55 -0800

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- prints num
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
