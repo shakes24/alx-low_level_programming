c55c41a365da104d7f8ff9dffdc0bcd471e4d67d
shakes24 <cherrycrisp0@gmail.com>
2022-12-09 13:58:45 -0800

    comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main- prints a combination of nums
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 48; x <= 56; x++)
+	{
+		for (y =49; y <=57; y++)
+		{
+		  if (x > y)
+		  {
+		   	putchar(x);
+			putchar(y);
+			if (x != 56 || y != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+	          }
+		 }
+	}
+	putchar('\n');
+
+	return (0);
+
+			
+	
+}
