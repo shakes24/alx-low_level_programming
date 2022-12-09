cd80d6efa8c2d4568d50dfb4c19b99d152756463
shakes24 <cherrycrisp0@gmail.com>
2022-12-09 09:30:50 -0800

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main- print baes 16
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int x;
+	char ch;
+
+	for (x = 48; x <= 58; x++)
+	{
+		putchar(x);
+	}
+
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
