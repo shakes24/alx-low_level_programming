f88bdf99b8f6c6a541322a6b93e851af282f5fbc
shakes24 <cherrycrisp0@gmail.com>
2022-12-09 10:19:29 -0800

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,11 +13,12 @@ int main(void)
 	for (x = 48; x <= 58; x++)
 	{
 		putchar(x);
-	}
+	
 
 	for (ch = 'a'; ch <= 'f'; ch++)
 	
-		putchar(ch); }
+		putchar(ch);
+       	}
 	putchar('\n');
 
 	return (0);
