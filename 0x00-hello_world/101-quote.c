700191fe024182d5cc851b83888b22c44a16d865
shakes24 <cherrycrisp0@gmail.com>
2022-12-09 00:39:53 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	write(5, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 		return (1);
 }
