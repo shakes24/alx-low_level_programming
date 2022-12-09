604813c5abdfcc4331a11c083649fa2209585e72
shakes24 <cherrycrisp0@gmail.com>
2022-12-09 08:46:58 -0800

    pnumbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- prints base 10 numbers
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int y;
+
+	for (y = 0; y < 10; y++)
+		putchar(y);
+
+	putchar('\n');
+
+	return (0);
+}
