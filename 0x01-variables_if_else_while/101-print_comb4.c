4a1d110a0257451e526676186e18ed5a192ed328
shakes24 <cherrycrisp0@gmail.com>
2022-12-09 15:00:03 -0800

    3comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h.
+
+/**
+ * main- prints the combination of 3 numbers
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int m, n, q;
+
+	for (m = 10; m <= 20; m++);
+	{
+		for (n =11; n <= 21; n++);
+		{
+			for (q = 12; q <= 22; q++);
+			{
+			 if (q > n > m)
+			{
+				putchar(m);
+				putchar(n);
+				putchar(q);
+			}
+			if (m != 20 || n!= 21 || q != 22)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
