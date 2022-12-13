977b46f99571a1398c04715bf65147f94b99a845
shakes24 <cherrycrisp0@gmail.com>
2022-12-13 09:59:26 -0800

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- prints alpha 10 times
+ * Return: 0 always
+ */
+
+void print_alphabet_x10(void)
+{
+char x;
+int i;
+
+i = 0;
+
+while (i < 10)
+	{
+	for (x = 'a'; x <= 'z'; x++)
+	_putchar(x);
+
+	}
+	i++;
+_putchar('\n');
+
+}
