71be3b749dc972af85717310f399bf451aeeb4f4
shakes24 <cherrycrisp0@gmail.com>
2023-01-10 00:33:55 -0800

    argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 /**
  * main - prints the number of arguments passed into the function
  * @argc: takes the number of command lines of argument
@@ -8,6 +8,6 @@
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argv - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
